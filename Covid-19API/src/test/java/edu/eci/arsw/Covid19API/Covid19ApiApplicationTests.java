4704c85a36a740448b8ae9987d4b15d084e0e687
David Caycedo <david.caycedo@mail.escuelaing.edu.co>
2020-03-25 00:49:25 -0500

    inicial

---
+++ Covid-19API/src/test/java/edu/eci/arsw/Covid19API/Covid19ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.Covid19API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
